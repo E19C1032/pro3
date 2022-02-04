7aa8de78d37dfedee789346c362057edeae50bfb
E19C1032 <82350462+E19C1032@users.noreply.github.com>
2022-02-04 20:32:12 +0900

    Delete pdo_connect_test.php

--- pdo_connect_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = null;
-try{
-    $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-    $conn = new PDO("mysql:host=127.0.0.1;dbname=test;charset=utf8;", "root", "7VK/=*TK+p-=>A%gSX6/YiDKSc=}3/k4", $option);
-}catch(PDOException $e){
-    die($e->getMessage());
-}
