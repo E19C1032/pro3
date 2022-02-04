32955f4d119fc888a1ef17b3119c351a5fead559
E19C1032 <82350462+E19C1032@users.noreply.github.com>
2022-02-04 20:22:55 +0900

    Delete js directory

--- js/checkBlank.js
+++
@@ -1,9 +0,0 @@
-/**
- * 文字列が空文字かどうか判定する。
- * @param {String} text テストする文字列。
- * @return {Boolean} 文字列が空白文字か？
- */
-const isBlank = text => {
-    if(!text.match(/\S/g)) return true;
-    return false;
-}
